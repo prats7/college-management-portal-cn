8d8bce875856597f64b9b53670fcc5b8165fb19a
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-23 01:48:56 +0530

    Update File Upload

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,12 +1,8 @@
 import React, { Fragment, useState } from 'react';
-
-
 import axios from 'axios';
-
 import Message from './Message';
 import Progress from './Progress';
 
-const links = [];
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
@@ -41,15 +37,6 @@ const FileUpload = () => {
             setUploadedFile({ fileName, filePath });
 
             setMessage('File Uploaded')
-            // links.push(filePath);
-            // links.forEach(showUploads);
-
-            // function showUploads(link, index) {
-            //     document.getElementById('uploads').innerHTML += `<div>
-            //     <a href={process.env.PUBLIC_URL + '/upload/bonfire.jpg } target="_blank">${link} </a>
-            //     </div>
-            //     `;
-            // }
         } catch (err) {
             if (err.response.status === 500) {
                 setMessage('There was a problem with the server');
@@ -71,15 +58,6 @@ const FileUpload = () => {
                 <input type="submit" color="dark" className="btn btn-primary mt-3" value="Upload" />
             </form>
 
-            <div className="row mt-5">
-                <div className="col-md-6 m-auto" id="uploads">
-                    <h4 >{uploadedFile.fileName}</h4>
-                    {/* <img style={{ width: '100%' }} src={uploadedFile.filePath} alt="" /> */}
-                    <a href="/upload/elastic-green-black-fluo.jpg" target="_blank">/upload/elastic-green-black-fluo.jpg</a>
-
-
-                </div>
-            </div>
 
         </Fragment>
     );
